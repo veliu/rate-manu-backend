c5a05e7c2b79893be5d397260e26877ccd58b513
Lirim Veliu <lirim@veliu.net>
2024-08-17 16:27:04 +0200

    Installed file system

--- src/Infra/Symfony/Filesystem/FileStorage.php
+++ src/Infra/Symfony/Filesystem/FileStorage.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Veliu\RateManu\Infra\Symfony\Filesystem;
 
-use DEPTRAC_INTERNAL\Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\String\Slugger\SluggerInterface;
